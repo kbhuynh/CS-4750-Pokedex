b8f13c3449a8fd152f7a66df1ce95388dd8984ae
Grace Wu <gw5cc@virginia.edu>
2020-04-23 18:05:10 -0400

    login page

--- index.php
+++ index.php
@@ -29,6 +29,9 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/index.php':
         require 'index.php';
         break;
+    case '/login.php':
+        require 'login.php';
+        break;
     case '/myCustom.php':
         require 'myCustom.php';
         break;
