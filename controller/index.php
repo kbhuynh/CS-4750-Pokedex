46a87df588df6e6c0fd2122c3475f286750acb59
Kevin Huynh <k.huynh1@yahoo.com>
2020-04-25 16:12:34 -0400

    editing correct index.php

--- controller/index.php
+++ controller/index.php
@@ -55,6 +55,12 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/teams.php':
         require '../teams.php';
         break;
+    case '/signin.php':
+        require '../signin.php';
+        break;
+    case '/logout.php':
+        require '../logout.php';
+        break;
     default:
         http_response_code(404);
         exit('Not Found');
